c26376aee4023b23e6f430dfddd75034f28cdec0
INFO-SUNIL <talk2me2005@gmail.com>
2022-09-08 15:39:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-11-new-mbq7zn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-11-new-mbq7zn)
---
+++ src/app/app.component.ts
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import {
+  Component,
+  Input,
+  OnInit,
+  OnDestroy,
+  OnChanges,
+  AfterViewInit,
+} from '@angular/core';
+import {
+  AtlasmapProvider,
+  IAtlasmapProviderProps,
+  Atlasmap,
+} from '@atlasmap/atlasmap';
+
+@Component({
+  selector: 'my-app',
+  template: `Hello<span [id]="'root'"></span>`,
+})
+export class AppComponent
+  implements OnInit, OnDestroy, OnChanges, AfterViewInit
+{
+  public rootDomID: string = 'root';
+
+  protected getRootDomNode() {
+    const node = document.getElementById(this.rootDomID);
+    return node;
+  }
+
+  protected getProps(): IAtlasmapProviderProps {
+    let baseJavaInspectionServiceUrl = '/v2/atlas/java/',
+      baseXMLInspectionServiceUrl = '/v2/atlas/xml/',
+      baseJSONInspectionServiceUrl = '/v2/atlas/json/',
+      baseCSVInspectionServiceUrl = '/v2/atlas/csv/',
+      baseMappingServiceUrl = '/v2/atlas/',
+      logLevel = 'info';
+
+    return {
+      baseJavaInspectionServiceUrl,
+      baseXMLInspectionServiceUrl,
+      baseJSONInspectionServiceUrl,
+      baseCSVInspectionServiceUrl,
+      baseMappingServiceUrl,
+      logLevel,
+    };
+  }
+
+  private isMounted(): boolean {
+    return !!this.rootDomID;
+  }
+
+  protected render() {
+    if (this.isMounted()) {
+      ReactDOM.render(
+        React.createElement(
+          AtlasmapProvider,
+          this.getProps(),
+          React.createElement(Atlasmap, {}, null)
+        ),
+        this.getRootDomNode()
+      );
+    }
+  }
+
+  ngOnInit() {
+    console.log(this.rootDomID);
+  }
+
+  ngOnChanges() {
+    this.render();
+  }
+
+  ngAfterViewInit() {
+    this.render();
+  }
+
+  ngOnDestroy() {
+    // Uncomment if Angular 4 issue that ngOnDestroy is called AFTER DOM node removal is resolved
+    // ReactDOM.unmountComponentAtNode(this.getRootDomNode())
+  }
+}
